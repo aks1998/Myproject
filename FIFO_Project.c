4f81e2b97b90e70fea52905c595df4c435d34a6d
aks1998 <31242658+aks1998@users.noreply.github.com>
2018-04-15 11:51:40 +0530

    Update FIFO_Project.c

--- FIFO_Project.c
+++ FIFO_Project.c
@@ -1,52 +1,78 @@
 #include<stdio.h>
-#include<unistd.h>
-#include<process.h>
+ 
+int findFIFO(int time[], int n){
+    int i, minimum = time[0], pos = 0;
+ 
+    for(i = 1; i < n; ++i){
+        if(time[i] < minimum){
+            minimum = time[i];
+            pos = i;
+        }
+    }
+    
+    return pos;
+}
+ 
 int main()
 {
-      int reference_string[10], page_faults = 0, m, n, s, pages, frames;
-      printf("\nEnter Total Number of Pages:\t");
-      scanf("%d", &pages);
-      printf("\nEnter values of Reference String:\n");
-      for(m = 0; m < pages; m++)
-      {
-            printf("Value No. [%d]:\t", m + 1);
-            scanf("%d", &reference_string[m]);
-      }
-      printf("\nEnter Total Number of Frames:\t");
-      {
-            scanf("%d", &frames);
-      }
-      int temp[frames];
-      for(m = 0; m < frames; m++)
-      {
-            temp[m] = -1;
-      }
-      for(m = 0; m < pages; m++)
-      {
-            s = 0;
-            for(n = 0; n < frames; n++)
-            {
-                  if(reference_string[m] == temp[n])
-                  {
-                        s++;
-                        page_faults--;
-                  }
-            }     
-            page_faults++;
-            if((page_faults <= frames) && (s == 0))
-            {
-                  temp[m] = reference_string[m];
-            }   
-            else if(s == 0)
-            {
-                  temp[(page_faults - 1) % frames] = reference_string[m];
-            }
-            printf("\n");
-            for(n = 0; n < frames; n++)
-            {     
-                  printf("%d\t", temp[n]);
-            }
-      } 
-      printf("\nTotal Page Faults:\t%d\n", page_faults);
-      return 0;
+    int no_of_frames, no_of_pages, frames[10], pages[30], counter = 0, time[10], flag1, flag2, i, j, pos, faults = 0;
+    printf("Enter number of frames: ");
+    scanf("%d", &no_of_frames);
+    
+    printf("Enter number of pages: ");
+    scanf("%d", &no_of_pages);
+    
+    printf("Enter reference string: ");
+    
+    for(i = 0; i < no_of_pages; ++i){
+        scanf("%d", &pages[i]);
+    }
+    
+    for(i = 0; i < no_of_frames; ++i){
+        frames[i] = -1;
+    }
+    
+    for(i = 0; i < no_of_pages; ++i){
+        flag1 = flag2 = 0;
+        
+        for(j = 0; j < no_of_frames; ++j){
+            if(frames[j] == pages[i]){
+                counter++;
+                time[j] = counter;
+                   flag1 = flag2 = 1;
+                   break;
+               }
+        }
+        
+        if(flag1 == 0){
+            for(j = 0; j < no_of_frames; ++j){
+                if(frames[j] == -1){
+                    counter++;
+                    faults++;
+                    frames[j] = pages[i];
+                    time[j] = counter;
+                    flag2 = 1;
+                    break;
+                }
+            }    
+        }
+        
+        if(flag2 == 0){
+            pos = findFIFO(time, no_of_frames);
+            counter++;
+            faults++;
+            frames[pos] = pages[i];
+            time[pos] = counter;
+        }
+        
+        printf("\n");
+        
+        for(j = 0; j < no_of_frames; ++j){
+            printf("%d\t", frames[j]);
+        }
+    }
+    
+    printf("\n\nTotal Page Faults = %d", faults);
+    
+    return 0;
 }
